4d7976a7209128d61d33e4f43284a20fefdedcfd
Yufan <yufan.zhao@hotmail.com>
2021-06-27 10:58:16 +0800

    去掉多余定义

--- src/apis/main/business.ts
+++ src/apis/main/business.ts
@@ -2,5 +2,5 @@ import { AbstractHttpUrl } from "src/models";
 
 export default class BusinessUrl implements AbstractHttpUrl
 {
-    public deviceManagePage: string = "/rjEquipmentManage/pageDetails";
+    public deviceManagePage = "/rjEquipmentManage/pageDetails";
 }
